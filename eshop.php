694e41a430d178ffea195bd55593236bb7e806c2
wjhamerling <w.j.hamerling@live.nl>
2019-01-24 15:00:28 +0100

    added explanation comments

--- eshop.php
+++ eshop.php
@@ -19,21 +19,23 @@
     //eshop.php
 
     include "db_connection.php";
-    
-   // $m_id = $_GET['Mario_ID']; 
 
-   // $sql_querie = "SELECT Mario_ID, Mario_Img, Mario_Name, Mario_Year, Mario_System FROM mario_games WHERE Mario_ID= '$m_id'"; 
+    if(isset($_GET['Mario_ID'])){
+        //ALS ?Mario_ID=x in de link staat.
+        $m_id = $_GET['Mario_ID'];
+        // $sql_querie = "SELECT Mario_ID, Mario_Img, Mario_Name, Mario_Year, Mario_System FROM mario_games WHERE Mario_ID= '$m_id'"; 
+    }else{
+        //ALS er niks in de link staat.
+        $sql_querie = "SELECT Mario_ID, Mario_Img, Mario_Name, Mario_Year, Mario_System FROM mario_games ";
+    }  
     
-   // $nummer = $_GET['Mario_ID']; 
-
-    $sql_querie = "SELECT Mario_ID, Mario_Img, Mario_Name, Mario_Year, Mario_System FROM mario_games";
     $db_result = $conn->query($sql_querie);  
 
     foreach ($db_result as $row)
     {            
         
         echo '<div class="flex">' .
-             '<a href="retrodump.php?Mario_ID=' . $row['Mario_ID'] . '">' .
+             '<a href="eshop.php?Mario_ID=' . $row['Mario_ID'] . '">' .
              '<img src="' . $row['Mario_Img'] . '" alt="' . $row['Mario_Img'] . '" style="width:150%">' .
              '</a>' .
              '<h1>' . $row['Mario_Name'] . '</h1>' .
