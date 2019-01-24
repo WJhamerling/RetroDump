fe3e1d5dde5fe38c6ff2cf6bfda0f6446a755a6b
wjhamerling <w.j.hamerling@live.nl>
2019-01-24 12:43:42 +0100

    update

--- card.php
+++ card.php
@@ -1,8 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>RETRO|DUMP</title>        
+        <link rel="stylesheet" type="text/css" media="screen" href="css/eshopheader.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="css/card.css" />
+        
+    </head>
+
+    <body>
+    <div class="header">
+<div class="left_segment"></div>
+<div class="middle_segment"></div>
+<div class="right_segment"></div>
+</div>
+
 <?php
 
     //CARD.PHP
 
-    include "db_connection.php";        
+    include "db_connection.php";
+    
+    // $nummer = $_GET['Mario_ID']; 
 
     $sql_querie = "SELECT Mario_ID, Mario_Img, Mario_Name, Mario_Year, Mario_System FROM mario_games";
     
@@ -13,13 +31,13 @@
         
         echo '<div class="flex">' .
              '<a href="retrodump.php?Mario_ID=' . $row['Mario_ID'] . '">' .
-             '<img src="' . $row['Mario_Img'] . '" alt="' . $row['Mario_Img'] . '" style="width:25%">' .
+             '<img src="' . $row['Mario_Img'] . '" alt="' . $row['Mario_Img'] . '" style="width:75%">' .
              '</a>' .
              '<h1>' . $row['Mario_Name'] . '</h1>' .
-             '<h4>' . $row['Mario_System'] . '</h4>' .
-             '<h5>' . $row['Mario_Year'] . '</h5>' .
+            // '<h4>' . $row['Mario_System'] . '</h4>' .
+             //'<h5>' . $row['Mario_Year'] . '</h5>' .
              
-             '<p class="title">' . $row['Mario_Name'] . '</p>' .           
+             //'<p class="title">' . $row['Mario_Name'] . '</p>' .           
              '<p></p>' .
              '</div>';
        
@@ -27,4 +45,5 @@
 
     $conn = null;
   
-?>
+?></body>
+</html>
--- retrodump.php
+++ retrodump.php
@@ -2,11 +2,12 @@
 <html>
     <head>
         <title>RETRO|DUMP</title>        
-        <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
-        
+        <link rel="stylesheet" type="text/css" media="screen" href="css/eshopheader.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="css/card.css" />
     </head>
 
     <body>
+
 <?php
 
     //retrodump.php
@@ -14,7 +15,6 @@
     include "db_connection.php";
     
     $m_id = $_GET['Mario_ID']; 
-
 
     $sql_querie = "SELECT Mario_ID, Mario_Img, Mario_Name, Mario_Year, Mario_System FROM mario_games WHERE Mario_ID= '$m_id'";
     
@@ -28,8 +28,8 @@
              '<img src="' . $row['Mario_Img'] . '" alt="' . $row['Mario_Img'] . '" style="width:150%">' .
              '</a>' .
              '<h1>' . $row['Mario_Name'] . '</h1>' .
-             '<h4>' . $row['Mario_System'] . '</h4>' .
-             '<h5>' . $row['Mario_Year'] . '</h5>' .
+            // '<h4>' . $row['Mario_System'] . '</h4>' .
+            //'<h5>' . $row['Mario_Year'] . '</h5>' .
              
              '<p class="title">' . $row['Mario_Name'] . '</p>' .           
              '<p></p>' .
